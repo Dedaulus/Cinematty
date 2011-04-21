fbf59dfe55929862a50ecb3a869361ae4e1c60ea
Dedaulus <dedaulus@gmail.com>
2011-04-21 17:59:05 +0400

    Refactoring

--- src/com/dedaulus/cinematty/activities/MovieActivity.java
+++ src/com/dedaulus/cinematty/activities/MovieActivity.java
@@ -8,7 +8,6 @@ import android.widget.Button;
 import android.widget.TextView;
 import com.dedaulus.cinematty.CinemattyApplication;
 import com.dedaulus.cinematty.R;
-import com.dedaulus.cinematty.framework.Movie;
 import com.dedaulus.cinematty.framework.MovieActor;
 import com.dedaulus.cinematty.framework.MovieGenre;
 
@@ -27,43 +26,18 @@ public class MovieActivity extends Activity {
         setContentView(R.layout.movie_info);
 
         mApp = (CinemattyApplication)getApplication();
-        Movie movie = mApp.getCurrentMovie();
 
-        TextView text = (TextView)findViewById(R.id.movie_caption);
-        text.setText(movie.getCaption());
+        setCaption();
 
-        text = (TextView)findViewById(R.id.movie_length);
-        String movieLength = movie.getLengthInMinutes() == 0 ? getString(R.string.unknown) : movie.getLengthInMinutes() + " " + getString(R.string.minutes);
-        text.setText(getString(R.string.movie_length) + ": " + movieLength);
+        setLength();
 
-        text = (TextView)findViewById(R.id.movie_genre);
-        if (movie.getGenres().size() > 0) {
-            StringBuilder genres = new StringBuilder(getString(R.string.genre) + ": ");
-            for (MovieGenre genre : movie.getGenres()) {
-                genres.append(genre.getGenre() + ", ");
-            }
-            genres.delete(genres.length() - 2, genres.length() - 1);
-            text.setText(genres.toString());
-        }
-        else {
-            text.setVisibility(View.GONE);
-        }
+        setGenre();
 
-        text = (TextView)findViewById(R.id.movie_actors);
-        if (movie.getActors().size() > 0) {
-            StringBuilder actors = new StringBuilder(getString(R.string.actors) + ": ");
-            for (MovieActor actor : movie.getActors()) {
-                actors.append(actor.getActor() + ", ");
-            }
-            actors.delete(actors.length() - 2, actors.length() - 1);
-            text.setText(actors.toString());
-        }
-        else {
-            text.setVisibility(View.GONE);
-        }
+        setActors();
 
-        text = (TextView)findViewById(R.id.movie_description);
-        text.setText(movie.getDescription());
+        setDescription();
+
+        setSchedule();
 
         Button btn = (Button)findViewById(R.id.show_schedules_button);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -71,36 +45,91 @@ public class MovieActivity extends Activity {
                 onSchedulesBtnClick(view);
             }
         });
+
         if (mApp.getCurrentCinema() == null) {
             btn.setText(getString(R.string.look_for_schedule));
-            //findViewById(R.id.advanced_movie_panel).setVisibility(View.VISIBLE);
-            findViewById(R.id.schedule_enum_for_one_cinema).setVisibility(View.GONE);
-
-            /*
-            Button btn = (Button)findViewById(R.id.show_schedules_button);
-            btn.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) {
-                    onSchedulesBtnClick(view);
-                }
-            });
-            */
         }
         else {
             btn.setText(getString(R.string.look_for_all_schedule));
-            //findViewById(R.id.advanced_movie_panel).setVisibility(View.GONE);
-            text = (TextView)findViewById(R.id.schedule_enum_for_one_cinema);
-            text.setVisibility(View.VISIBLE);
+        }
+    }
 
-            List<String> showTime = mApp.getCurrentCinema().getShowTimes().get(movie);
+    private void setCaption() {
+        TextView text = (TextView)findViewById(R.id.movie_caption);
+        text.setText(mApp.getCurrentMovie().getCaption());
+    }
+
+    private void setSchedule() {
+        TextView text = (TextView)findViewById(R.id.schedule_enum_for_one_cinema);
+
+        if (mApp.getCurrentCinema() != null) {
+            List<String> showTime = mApp.getCurrentCinema().getShowTimes().get(mApp.getCurrentMovie());
             if (showTime != null) {
                 StringBuilder times = new StringBuilder();
-                    for (String str : showTime) {
-                        times.append(str + ", ");
-                    }
-                    times.delete(times.length() - 2, times.length() - 1);
-                    text.setText(times.toString());
+                for (String str : showTime) {
+                    times.append(str + ", ");
+                }
+                times.delete(times.length() - 2, times.length() - 1);
+                text.setText(times.toString());
             }
 
+            findViewById(R.id.movie_schedule_enum_panel).setVisibility(View.VISIBLE);
+        } else {
+            findViewById(R.id.movie_schedule_enum_panel).setVisibility(View.GONE);
+        }
+    }
+
+    private void setLength() {
+        TextView text = (TextView)findViewById(R.id.movie_length);
+        if (mApp.getCurrentMovie().getLengthInMinutes() != 0) {
+            text.setText(mApp.getCurrentMovie().getLengthInMinutes() + " " + getString(R.string.minutes));
+
+            findViewById(R.id.movie_length_panel).setVisibility(View.VISIBLE);
+        } else {
+            findViewById(R.id.movie_length_panel).setVisibility(View.GONE);
+        }
+    }
+
+    private void setGenre() {
+        TextView text = (TextView)findViewById(R.id.movie_genre);
+        if (mApp.getCurrentMovie().getGenres().size() != 0) {
+            StringBuilder genres = new StringBuilder();
+            for (MovieGenre genre : mApp.getCurrentMovie().getGenres()) {
+                genres.append(genre.getGenre() + ", ");
+            }
+            genres.delete(genres.length() - 2, genres.length() - 1);
+            text.setText(genres.toString());
+
+            findViewById(R.id.movie_genre_panel).setVisibility(View.VISIBLE);
+        } else {
+            findViewById(R.id.movie_genre_panel).setVisibility(View.GONE);
+        }
+    }
+
+    private void setActors() {
+        TextView text = (TextView)findViewById(R.id.movie_actors);
+        if (mApp.getCurrentMovie().getActors().size() != 0) {
+            StringBuilder actors = new StringBuilder();
+            for (MovieActor actor : mApp.getCurrentMovie().getActors()) {
+                actors.append(actor.getActor() + ", ");
+            }
+            actors.delete(actors.length() - 2, actors.length() - 1);
+            text.setText(actors.toString());
+
+            findViewById(R.id.movie_actors_panel).setVisibility(View.VISIBLE);
+        } else {
+            findViewById(R.id.movie_actors_panel).setVisibility(View.GONE);
+        }
+    }
+
+    private void setDescription() {
+        TextView text = (TextView)findViewById(R.id.movie_description);
+        if (mApp.getCurrentMovie().getDescription().length() != 0) {
+            text.setText(mApp.getCurrentMovie().getDescription());
+
+            findViewById(R.id.movie_description_panel).setVisibility(View.VISIBLE);
+        } else {
+            findViewById(R.id.movie_description_panel).setVisibility(View.GONE);
         }
     }
 
