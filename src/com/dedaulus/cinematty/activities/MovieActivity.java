c81179793061088f9195b2efa5a8764781fa9f07
Dedaulus <dedaulus@gmail.com>
2011-04-24 01:20:26 +0400

    Fixed bug - app raised an exception when trying to set favourite cinema after viewing movie info

--- src/com/dedaulus/cinematty/activities/MovieActivity.java
+++ src/com/dedaulus/cinematty/activities/MovieActivity.java
@@ -3,6 +3,7 @@ package com.dedaulus.cinematty.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -52,6 +53,14 @@ public class MovieActivity extends Activity {
         else {
             btn.setText(getString(R.string.look_for_all_schedule));
         }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            mApp.setCurrentMovie(null);
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
     private void setCaption() {
