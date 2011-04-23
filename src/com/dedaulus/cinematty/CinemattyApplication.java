a997f3987397db562b401e54131297aebf6fa4b2
Dedaulus <dedaulus@gmail.com>
2011-04-23 17:50:29 +0400

    Added favourite cinemas storing and restoring logic

--- src/com/dedaulus/cinematty/CinemattyApplication.java
+++ src/com/dedaulus/cinematty/CinemattyApplication.java
@@ -1,6 +1,7 @@
 package com.dedaulus.cinematty;
 
 import android.app.Application;
+import android.content.SharedPreferences;
 import com.dedaulus.cinematty.framework.Cinema;
 import com.dedaulus.cinematty.framework.Movie;
 import com.dedaulus.cinematty.framework.MovieActor;
@@ -8,6 +9,8 @@ import com.dedaulus.cinematty.framework.MovieGenre;
 import com.dedaulus.cinematty.framework.tools.DefaultComparator;
 import com.dedaulus.cinematty.framework.tools.ScheduleReceiver;
 import com.dedaulus.cinematty.framework.tools.UniqueSortedList;
+
+import java.util.Map;
 
 /*
  * User: Dedaulus
@@ -24,6 +27,9 @@ public class CinemattyApplication extends Application {
     private MovieActor mCurrentActor;
     private MovieGenre mCurrentGenre;
 
+    private static final String FAV_CINEMAS_FILE = "cinematty_fav_cinemas";
+    private static final String FAV_CINEMAS_PREFERENCE = "fav_cinemas";
+
     {
         mCinemas = new UniqueSortedList<Cinema>(new DefaultComparator<Cinema>());
         mMovies = new UniqueSortedList<Movie>(new DefaultComparator<Movie>());
@@ -32,9 +38,16 @@ public class CinemattyApplication extends Application {
     }
 
     public void retrieveData() {
-        ScheduleReceiver so = new ScheduleReceiver(getString(R.string.default_url));
+        ScheduleReceiver receiver = new ScheduleReceiver(getString(R.string.default_url));
+        receiver.getSchedule(mCinemas, mMovies, mActors, mGenres);
 
-        so.getSchedule(mCinemas, mMovies, mActors, mGenres);
+        Map<String, ?> favs = getFavouriteCinemas();
+        for (String caption : favs.keySet()) {
+            int cinemaId = mCinemas.indexOf(new Cinema(caption));
+            if (cinemaId != -1) {
+                mCinemas.get(cinemaId).setFavourite(((Long)favs.get(caption)).longValue());
+            }
+        }
     }
 
     public boolean isUpToDate() {
@@ -88,5 +101,24 @@ public class CinemattyApplication extends Application {
 
     public MovieGenre getCurrentGenre() {
         return mCurrentGenre;
+    }
+
+    public void saveFavouriteCinemas() {
+        SharedPreferences preferences = getSharedPreferences(FAV_CINEMAS_FILE, MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        for (Cinema cinema : mCinemas) {
+            if (cinema.getFavourite() > 0) {
+                editor.putLong(cinema.getCaption(), cinema.getFavourite());
+            } else {
+                editor.remove(cinema.getCaption());
+            }
+        }
+
+        editor.commit();
+    }
+
+    private Map<String, ?> getFavouriteCinemas() {
+        SharedPreferences preferences = getSharedPreferences(FAV_CINEMAS_FILE, MODE_PRIVATE);
+        return preferences.getAll();
     }
 }
