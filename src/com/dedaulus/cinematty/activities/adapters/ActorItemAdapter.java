5ca7f3a48da21cab7f3bf7e67d39242d34ddc2d9
Dedaulus <dedaulus@gmail.com>
2011-10-02 06:16:38 +0400

    Added QuickAction menu for actors

--- src/com/dedaulus/cinematty/activities/adapters/ActorItemAdapter.java
+++ src/com/dedaulus/cinematty/activities/adapters/ActorItemAdapter.java
@@ -1,6 +1,8 @@
 package com.dedaulus.cinematty.activities.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +11,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import com.dedaulus.cinematty.R;
 import com.dedaulus.cinematty.framework.MovieActor;
+import net.londatiga.android.ActionItem;
+import net.londatiga.android.QuickAction;
 
 import java.util.Collections;
 import java.util.Comparator;
@@ -22,10 +26,15 @@ import java.util.List;
 public class ActorItemAdapter extends BaseAdapter implements SortableAdapter<MovieActor> {
     private Context mContext;
     private List<MovieActor> mActors;
+    private QuickAction mQuickAction;
+    private String mCurrentActorName;
 
     public ActorItemAdapter(Context context, List<MovieActor> actors) {
         mContext = context;
         mActors = actors;
+        mCurrentActorName = null;
+        mQuickAction = new QuickAction(mContext);
+        setQuickActions(this);
     }
 
     public int getCount() {
@@ -54,6 +63,18 @@ public class ActorItemAdapter extends BaseAdapter implements SortableAdapter<Mov
         } else {
             image.setImageResource(android.R.drawable.btn_star_big_off);
         }
+        image.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                onActorFavIconClick(view);
+            }
+        });
+
+        image = (ImageView)view.findViewById(R.id.more_icon_in_actor_list);
+        image.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                onMoreIconClick(view);
+            }
+        });
 
         TextView text = (TextView)view.findViewById(R.id.actor_caption_in_actor_list);
         text.setText(actor.getActor());
@@ -76,5 +97,73 @@ public class ActorItemAdapter extends BaseAdapter implements SortableAdapter<Mov
     public void sortBy(Comparator<MovieActor> actorComparator) {
         Collections.sort(mActors, actorComparator);
         notifyDataSetChanged();
+    }
+
+    private void onActorFavIconClick(View view) {
+        View parent = (View)view.getParent();
+        TextView caption = (TextView)parent.findViewById(R.id.actor_caption_in_actor_list);
+
+        int actorId = mActors.indexOf(new MovieActor(caption.getText().toString()));
+        if (actorId != -1) {
+            MovieActor actor = mActors.get(actorId);
+
+            if (actor.getFavourite() > 0) {
+                actor.setFavourite(false);
+                ((ImageView)view).setImageResource(android.R.drawable.btn_star_big_off);
+            } else {
+                actor.setFavourite(true);
+                ((ImageView)view).setImageResource(android.R.drawable.btn_star_big_on);
+            }
+        }
+    }
+
+    private void setQuickActions(ActorItemAdapter adapter) {
+        ActionItem googleAction = new ActionItem(mContext.getResources().getDrawable(R.drawable.ic_more_google));
+        googleAction.setTitle(mContext.getString(R.string.google));
+
+        ActionItem yandexAction = new ActionItem(mContext.getResources().getDrawable(R.drawable.ic_more_yandex));
+        yandexAction.setTitle(mContext.getString(R.string.yandex));
+
+        ActionItem wikiAction = new ActionItem(mContext.getResources().getDrawable(R.drawable.ic_more_wiki));
+        wikiAction.setTitle(mContext.getString(R.string.wiki));
+
+        mQuickAction.addActionItem(googleAction);
+        mQuickAction.addActionItem(yandexAction);
+        mQuickAction.addActionItem(wikiAction);
+
+        final ActorItemAdapter aia = adapter;
+
+        mQuickAction.setOnActionItemClickListener(new QuickAction.OnActionItemClickListener() {
+            public void onItemClick(int pos) {
+                aia.onMoreItemClick(pos);
+            }
+        });
+    }
+
+    private void onMoreItemClick(int pos) {
+        String url;
+        switch (pos) {
+        case 1:
+            url = mContext.getString(R.string.yandex_search_url);
+            break;
+        case 2:
+            url = mContext.getString(R.string.wiki_search_url);
+            break;
+        case 0:
+        default:
+            url = mContext.getString(R.string.google_search_url);
+            break;
+        }
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url + mCurrentActorName));
+        mContext.startActivity(intent);
+    }
+
+    public void onMoreIconClick(View view) {
+        View parent = (View)view.getParent();
+        TextView caption = (TextView)parent.findViewById(R.id.actor_caption_in_actor_list);
+        mCurrentActorName = caption.getText().toString();
+
+        mQuickAction.show(view);
     }
 }
