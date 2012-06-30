d880746a4e43143d5e48a0660cadeb8666c98a03
Dedaulus <dedaulus@gmail.com>
2012-06-30 22:07:43 +0400

    Google added new maps!

--- src/com/dedaulus/cinematty/activities/CinemaMapView.java
+++ src/com/dedaulus/cinematty/activities/CinemaMapView.java
@@ -52,7 +52,7 @@ public class CinemaMapView extends MapActivity {
         mapView.setBuiltInZoomControls(true);
         // CRUNCH!
         Set<String> poorCities = new HashSet<String>(
-                Arrays.asList("мурманск", "тюмень", "иркутск", "омск", "владивосток"));
+                Arrays.asList("ростов-на-дону", "тюмень", "красноярск", "саратов", "владивосток"));
         String cityName = app.getCurrentCity().getName();
         if (poorCities.contains(cityName.toLowerCase())) {
             mapView.setSatellite(true);
