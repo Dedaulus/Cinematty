de7ed1c131b37ca47238208107790cd04476c551
dedaulus <dedaulus@gmail.com>
2013-03-03 01:54:38 +0400

    Сургут has no satellite view

--- src/com/dedaulus/cinematty/activities/CinemaMapView.java
+++ src/com/dedaulus/cinematty/activities/CinemaMapView.java
@@ -52,7 +52,7 @@ public class CinemaMapView extends MapActivity {
         mapView.setBuiltInZoomControls(true);
         // CRUNCH!
         Set<String> poorCities = new HashSet<String>(
-                Arrays.asList("петрозаводск", "южно-сахалинск"));
+                Arrays.asList("петрозаводск", "южно-сахалинск", "сургут"));
         String cityName = app.getCurrentCity().getName();
         if (poorCities.contains(cityName.toLowerCase())) {
             mapView.setSatellite(true);
